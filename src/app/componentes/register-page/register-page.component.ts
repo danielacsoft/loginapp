26371a94e0f168e8caa7752ff00abad77ca4e13d
danielacsoft <daguirre@acsoft.com.ar>
2018-02-27 23:20:00 -0300

    creamos servicios y registro usuairos

--- src/app/componentes/register-page/register-page.component.ts
+++ src/app/componentes/register-page/register-page.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '../../servicios/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
   styleUrls: ['./register-page.component.scss']
 })
 export class RegisterPageComponent implements OnInit {
-
-  constructor() { }
+  public email: string;
+  public password: string;
+  public router: Router;
+  constructor(
+    public authService: AuthService
+  ) {}
 
   ngOnInit() {
   }
-
+  onSubmitAddUser(){
+    this.authService.registeruser(this.email,this.password)
+    .then( (res) =>{
+     this.router.navigate(['/privado']);
+    }).catch( (err) =>{
+       console.log(err);
+    })
+  }
 }
